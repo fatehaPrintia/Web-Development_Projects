6b02308b8d8d11c4f0964d759219a576f94bf63b
fatehaPrintia <80954042+fatehaPrintia@users.noreply.github.com>
2023-01-14 18:29:23 +0600

    Update script.js

--- Spotify clone/script.js
+++ Spotify clone/script.js
@@ -1,6 +1,6 @@
 console.log("welcome to spotify");
 let songIndex= 0;
-let audioElement= new Audio('songs/2.mp3');
+let audioElement= new Audio('songs/1.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar =document.getElementById('myProgressBar');
 let gif =document.getElementById('gif');
